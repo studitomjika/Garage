3d2210ba127d3baac6238b74d945ee64800296ea
StudiTomjika <studitomjika@protonmail.com>
2024-04-10 18:28:41 +0200

    Controller homepage créé

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        /* return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]); */
+
+        return new Response(<<<EOF
+            <html>
+                <body>
+                    <h1>coucou!</h1>
+                        <p>couroucoucou</p>
+                </body>
+            </html>
+            EOF
+        );
+    }
+}
