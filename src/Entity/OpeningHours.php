9aec698393bced1abdc01024127593cba712b173
StudiTomjika <studitomjika@protonmail.com>
2024-04-12 11:54:17 +0200

    BDD update with non-relational classes

---
+++ src/Entity/OpeningHours.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OpeningHoursRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OpeningHoursRepository::class)]
+class OpeningHours
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $day = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $hours = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDay(): ?string
+    {
+        return $this->day;
+    }
+
+    public function setDay(string $day): static
+    {
+        $this->day = $day;
+
+        return $this;
+    }
+
+    public function getHours(): ?string
+    {
+        return $this->hours;
+    }
+
+    public function setHours(?string $hours): static
+    {
+        $this->hours = $hours;
+
+        return $this;
+    }
+}
