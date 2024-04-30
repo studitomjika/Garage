67443e65dfe2e323a9a29c5b23f2e252f1f51182
StudiTomjika <studitomjika@protonmail.com>
2024-04-30 16:28:17 +0200

    apiPlatformFilter

--- public/filters.js
+++ public/filters.js
@@ -34,4 +34,44 @@ async function requestFilteredCarDeals(){
   })
 }
 
-requestFilteredCarDeals();
+requestFilteredCarDeals_viaAPI();
+
+let carDeals_array = []
+
+async function requestFilteredCarDeals_viaAPI(){
+
+  const parameters = {
+    price: "%5Bbetween%5D=0..90000",
+    kilometers: "%5Bbetween%5D=40000..90000",
+    year: "%5Bbetween%5D=0..90000"
+  };
+
+  await fetch("https://127.0.0.1:8000/api/voiture_d_occasions?" + new URLSearchParams(parameters), {
+    method: "GET",
+    headers: {
+      'credentials': 'same-origin',
+      'X-Requested-With': 'XMLHttpRequest',
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json().then(data => ({status: response.status, body: data})))
+  .then(obj =>  carDeals_array = obj.body["hydra:member"]);
+  
+  for (const carDeal of carDeals_array)
+  {
+    console.log(carDeal)
+    carDealSection.innerHTML += 
+
+    "<div class=\'car-deal show\'><div class=\'photo\'>\
+      <img src= \'/img\/" + carDeal.pictureFilename + "' alt=\'\' width=\'100\%\' height=\'100\%\'>\
+      </div>\
+      <div class=\'twoline\'>\
+          <p class=\'" + carDeal.model + "\'>model</p>\
+      </div>\
+      <p class=\'km\'>" + carDeal.kilometers + " km</p>\
+      <p class=\'year\'>" + carDeal.year + "</p>\
+      <p class=\'price\'>" + carDeal.price +" €</p>\
+      <p class=\'id_car_deal hidden\'>" + carDeal.id + "</p>\
+    </div>"
+  }
+}
--- src/Entity/UsedCar.php
+++ src/Entity/UsedCar.php
@@ -7,32 +7,50 @@ use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter\RangeFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: UsedCarRepository::class)]
-#[ApiResource]
-//#[ApiFilter(SearchFilter::class, properties: ['id' => 'exact', 'price' => 'exact', 'kilometers' => 'exact', 'year' => 'exact'])]
+#[ApiResource(
+    shortName: 'Voiture d\'occasion',
+    description: 'Trouver des voitures d\'occasion',
+    operations: [
+        new Get(normalizationContext: ['groups' => 'usedcar:item']),
+        new GetCollection(normalizationContext: ['groups' => 'usedcar:list'])
+    ],
+    paginationEnabled: false,
+)]
 class UsedCar
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['usedcar:list', 'usedcar:item'])]
     private ?int $id = null;
 
+    #[Groups(['usedcar:list', 'usedcar:item'])]
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $model = null;
 
+    #[Groups(['usedcar:list', 'usedcar:item'])]
+    #[ApiFilter(RangeFilter::class)]
     #[ORM\Column(nullable: true)]
     private ?int $kilometers = null;
 
+    #[Groups(['usedcar:list', 'usedcar:item'])]
+    #[ApiFilter(RangeFilter::class)]
     #[ORM\Column(nullable: true)]
     private ?int $year = null;
 
+    #[Groups(['usedcar:list', 'usedcar:item'])]
     #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2, nullable: true)]
+    #[ApiFilter(RangeFilter::class)]
     private ?string $price = null;
 
+    #[Groups(['usedcar:list', 'usedcar:item'])]
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $pictureFilename = null;
 
